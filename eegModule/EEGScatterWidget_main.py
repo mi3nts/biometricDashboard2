8e77db8218a1faa59aeaef4af35b5c458c5bb384
William <48535210+WilliamUTD@users.noreply.github.com>
2020-04-06 15:26:05 -0500

    Updated formats

--- eegModule/EEGScatterWidget_main.py
+++ eegModule/EEGScatterWidget_main.py
@@ -12,7 +12,7 @@ from pylsl import StreamInlet, resolve_stream
 from pyqtgraph import PlotWidget, plot
 # from PyQtAlphaFrequency import AlphaFrequencyPG
 # from PyQtThetaFrequency import ThetaFrequencyPG
-from EEGScatter_submodule_graph import EEGGraph
+from EEGScatter_submodule_graph import EEG_Graph_Submodule
 from MatPlotLibCmapToPyQtColorMap import cmapToColormap
 
 import pyqtgraph as pg
@@ -32,202 +32,205 @@ import time
 
 
 class MainWindow(QMainWindow):
+	def __init__(self, *args, **kwargs):
+		super(MainWindow, self).__init__(*args, **kwargs)
 
-    def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
+		# set title of application window
+		self.setWindowTitle("Biometric Dashboard")
 
-        # set title of application window
-        self.setWindowTitle("Biometric Dashboard")
+		# # resize main window
+		self.resize(1600, 1200)
 
-        # # resize main window
-        self.resize(1600, 1200)
+		# create a window widget for main window
+		widget = QWidget()
 
-        # create a window widget for main window
-        widget = QWidget()
+		# define a layout for window
+		layout = QGridLayout()
 
-        # define a layout for window
-        layout = QGridLayout()
+		# add all modules to MainWindow where EEG module takes up 1 row and 2
+		# columns and sits in the top left grid box. The respiration module sits
+		# in the bottom left grid box. The temperature module sits in the bottom
+		# right grid box.
+		layout.addWidget(EEGmodule_main(), 0, 0, 1, 2)
+		layout.addWidget(pg.GraphicsLayoutWidget(), 1, 0)
 
-        # add all modules to MainWindow where EEG module takes up 1 row and 2
-        # columns and sits in the top left grid box. The respiration module sits
-        # in the bottom left grid box. The temperature module sits in the bottom
-        # right grid box.
-        layout.addWidget(EEGmodule_main(), 0, 0, 1, 2)
-        layout.addWidget(pg.GraphicsLayoutWidget(), 1, 0)
-        layout.addWidget(CmapImage(), 1, 1)
+		#layout.addWidget(, 1, 1)
 
-        #layout.addWidget(DeltaFrequencyPG(), 1, 1)
+		#layout.addWidget(DeltaFrequencyPG(), 1, 1)
 
-        # add layout to window Widget
-        widget.setLayout(layout)
+		# add layout to window Widget
+		widget.setLayout(layout)
 
-        # Set the central widget of the Window. Widget will expand
-        # to take up all the space in the window by default.
-        self.setCentralWidget(widget)
+		# Set the central widget of the Window. Widget will expand
+		# to take up all the space in the window by default.
+		self.setCentralWidget(widget)
 
-        # change window style to dark mode
-        darkMode()
+		# change window style to dark mode
+		darkMode()
 
 
 class CmapImage(QWidget):
 
-    def __init__(self):
-        super().__init__()
+	def __init__(self):
+		super().__init__()
 
-        self.im = QPixmap("./jet.png")
+		self.im = QPixmap("./jet.png")
 
-        self.label = QLabel()
-        self.im = self.im.scaled(
-            300, 350, Qt.KeepAspectRatio, Qt.FastTransformation)
-        self.label.setPixmap(self.im)
-        self.grid = QGridLayout()
-        self.grid.addWidget(self.label, 0, 1)
-        self.setLayout(self.grid)
+		self.label = QLabel()
+		self.im = self.im.scaled(
+			300, 350, Qt.KeepAspectRatio, Qt.FastTransformation)
+		self.label.setPixmap(self.im)
+		self.grid = QGridLayout()
+		self.grid.addWidget(self.label, 0, 1)
+		self.setLayout(self.grid)
 
-        # self.setGeometry(100, 100, 800, 300)
+		# self.setGeometry(100, 100, 800, 300)
 
-        # self.setWindowTitle("PyQT show image")
-        self.show()
+		# self.setWindowTitle("PyQT show image")
+		self.show()
 
 
 # create class to contain EEG module
 class EEGmodule_main(QGroupBox):
     # initialize attributes of EEGmodule class
-    def __init__(self, *args, **kwargs):
-        # have EEGmodule inherit attributes of QGroupBox
-        super(QGroupBox, self).__init__(*args, **kwargs)
+	def __init__(self, *args, **kwargs):
+		# have EEGmodule inherit attributes of QGroupBox
+		super(QGroupBox, self).__init__(*args, **kwargs)
 
-        # set title of EEGmodule
-        self.setTitle("EEG Module")
-        # create layout for EEG Module
-        self.layout = QGridLayout()
-        # set layout for module
-        self.setLayout(self.layout)
+		# set title of EEGmodule
+		self.setTitle("EEG Module")
+		# create layout for EEG Module
+		self.layout = QGridLayout()
+		# set layout for module
+		self.setLayout(self.layout)
 
-        # Creating graphs
-        self.alphaGraph = EEGGraph()
-        self.alphaGraph.setGraphTitle("Alpha Band")
-        self.alphaBand = -3
-        ######################################
-        self.thetaGraph = EEGGraph()
-        self.thetaGraph.setGraphTitle("Theta Band")
-        self.thetaBand = -2
-        ######################################
-        self.deltaGraph = EEGGraph()
-        self.deltaGraph.setGraphTitle("Delta Band")
-        self.deltaBand = -1
-        #######################################
+		# Creating graphs
+		self.alphaGraph = EEG_Graph_Submodule()
+		self.alphaGraph.setGraphTitle("Alpha Band (8-12Hz)")
+		self.alphaBand = -3
+		self.alphaGraph.resize(200,200)
+		######################################
+		self.thetaGraph = EEG_Graph_Submodule()
+		self.thetaGraph.setGraphTitle("Theta Band (4-7Hz)")
+		self.thetaBand = -2
+		self.thetaGraph.resize(200,200)
+		######################################
+		self.deltaGraph = EEG_Graph_Submodule()
+		self.deltaGraph.setGraphTitle("Delta Band (0-4Hz)")
+		self.deltaBand = -1
+		self.deltaGraph.resize(200,200)
+		#######################################
 
-        # checkbox for alphaGraph
-        alphabox = QCheckBox("alphaGraph band", self)
-        alphabox.setChecked(True)
-        alphabox.stateChanged.connect(lambda: self.hideGraph(button=alphabox))
+		# checkbox for alphaGraph
+		alphabox = QCheckBox("Alpha Band", self)
+		alphabox.setChecked(True)
+		alphabox.stateChanged.connect(lambda: self.hideGraph(button=alphabox))
 
-        thetaBox = QCheckBox("Theta Band", self)
-        thetaBox.setChecked(True)
-        thetaBox.stateChanged.connect(lambda: self.hideGraph(button=thetaBox))
-        thetaBox.move(100, 0)
+		thetaBox = QCheckBox("Theta Band", self)
+		thetaBox.setChecked(True)
+		thetaBox.stateChanged.connect(lambda: self.hideGraph(button=thetaBox))
+		thetaBox.move(100, 0)
 
-        deltaBox = QCheckBox("Delta band", self)
-        deltaBox.setChecked(True)
-        deltaBox.stateChanged.connect(lambda: self.hideGraph(button=deltaBox))
-        deltaBox.move(200, 0)
+		deltaBox = QCheckBox("Delta Band", self)
+		deltaBox.setChecked(True)
+		deltaBox.stateChanged.connect(lambda: self.hideGraph(button=deltaBox))
+		deltaBox.move(200, 0)
 
-        # add a simple label widget to layout
-        # self.layout.addWidget(self.deltaGraph)
-        # self.layout.addWidget(self.thetaGraph)
-        # self.layout.addWidget(self.alphaGraph)
-        # grid
-        self.layout.addWidget(self.deltaGraph, 0, 1, 1, 1)
-        self.layout.addWidget(self.thetaGraph, 0, 2, 1, 1)
-        self.layout.addWidget(self.alphaGraph, 0, 0, 1, 1)
+		# add a simple label widget to layout
+		# self.layout.addWidget(self.deltaGraph)
+		# self.layout.addWidget(self.thetaGraph)
+		# self.layout.addWidget(self.alphaGraph)
+		# grid
+		self.layout.addWidget(self.deltaGraph, 0, 0, 1, 1)
+		self.layout.addWidget(self.thetaGraph, 0, 1, 1, 1)
+		self.layout.addWidget(self.alphaGraph, 0, 2, 1, 1)
+		self.layout.addWidget(CmapImage(), 0,3,1,1)
+		# get the node positions
+		x, y, nodeList = EEGArray()
 
-        # get the node positions
-        x, y, nodeList = EEGArray()
+		# set cmap
+		cmap = getattr(matplotlib.cm, 'jet')
+		self.color_list = cmapToColormap(cmap)
+		ticks = []
+		colors = []
+		for item in self.color_list:
+			ticks.append(item[0])
+			colors.append(item[1])
 
-        # set cmap
-        cmap = getattr(matplotlib.cm, 'jet')
-        self.color_list = cmapToColormap(cmap)
-        ticks = []
-        colors = []
-        for item in self.color_list:
-            ticks.append(item[0])
-            colors.append(item[1])
+		self.pgCM = pg.ColorMap(pos=ticks, color=colors)
+		print(type(self.pgCM))
+		# define number of electrodes
+		self.n = 64
+		# initialize newdata
+		self.newdata = np.zeros(self.n)
 
-        self.pgCM = pg.ColorMap(pos=ticks, color=colors)
-        print(type(self.pgCM))
-        # define number of electrodes
-        self.n = 64
-        # initialize newdata
-        self.newdata = np.zeros(self.n)
+		# initialize 64 by 64 data array
+		self.data = np.zeros((self.n, self.n))
 
-        # initialize 64 by 64 data array
-        self.data = np.zeros((self.n, self.n))
+		# get global max for normalization
+		self.aglobalMax = -(sys.maxsize)-1
+		self.tglobalMax = -(sys.maxsize)-1
+		self.dglobalMax = -(sys.maxsize)-1
 
-        # get global max for normalization
-        self.aglobalMax = -(sys.maxsize)-1
-        self.tglobalMax = -(sys.maxsize)-1
-        self.dglobalMax = -(sys.maxsize)-1
+		# Open StreamInlet
+		print("looking for an EEG stream...")
+		self.streams = resolve_stream()
 
-        # Open StreamInlet
-        print("looking for an EEG stream...")
-        self.streams = resolve_stream()
+		self.inlet = StreamInlet(self.streams[0])
 
-        self.inlet = StreamInlet(self.streams[0])
+		# self.view = pg.GraphicsItem()
+		# gradientItem = pg.GradientItemEditor()
+		# self.view.addItem(self.Colormap.getGradient().setOrientation('right'))
+		# self.layout.addWidget(self.view)
 
-        # self.view = pg.GraphicsItem()
-        # gradientItem = pg.GradientItemEditor()
-        # self.view.addItem(self.Colormap.getGradient().setOrientation('right'))
-        # self.layout.addWidget(self.view)
+		# create timer
+		self.timer = QTimer(self)
+		# self.timer.setInterval(10000)
+		self.timer.timeout.connect(self.PullData)
+		self.timer.start(20)
 
-        # create timer
-        self.timer = QTimer(self)
-        # self.timer.setInterval(10000)
-        self.timer.timeout.connect(self.PullData)
-        self.timer.start(20)
+		self.count = 0
 
-        self.count = 0
+	def PullData(self):
 
-    def PullData(self):
+		starttime = time.time()
 
-        starttime = time.time()
+		self.count = self.count+1
+		# if self.count == 50:
+		# self.inlet = StreamInlet(self.streams[0])
 
-        self.count = self.count+1
-        # if self.count == 50:
-        # self.inlet = StreamInlet(self.streams[0])
+		# pull data
+		sample = self.inlet.pull_sample()
+		self.newdata = np.asarray(sample[0][:self.n])
+		# print(timestamp)
 
-        # pull data
-        sample = self.inlet.pull_sample()
-        self.newdata = np.asarray(sample[0][:self.n])
-        # print(timestamp)
+		for i in range(4):
+			if i == 1:
+				temp, self.dglobalMax, self.data = getCmapByFreqVal(
+					self.data, self.newdata, self.deltaBand, self.dglobalMax)
+				# set colors
+				acolors = self.pgCM.map(temp)
+				self.deltaGraph.update_nodes(colors=acolors)
 
-        for i in range(4):
-            if i == 1:
-                temp, self.dglobalMax, self.data = getCmapByFreqVal(
-                    self.data, self.newdata, self.deltaBand, self.dglobalMax)
-                # set colors
-                acolors = self.pgCM.map(temp)
-                self.deltaGraph.update_nodes(colors=acolors)
+			if i == 2:
+				temp, self.tglobalMax, self.data = getCmapByFreqVal(
+					self.data, self.newdata, self.thetaBand, self.tglobalMax)
+				# set colors
+				bcolors = self.pgCM.map(temp)
+				self.thetaGraph.update_nodes(colors=bcolors)
 
-            if i == 2:
-                temp, self.tglobalMax, self.data = getCmapByFreqVal(
-                    self.data, self.newdata, self.thetaBand, self.tglobalMax)
-                # set colors
-                bcolors = self.pgCM.map(temp)
-                self.thetaGraph.update_nodes(colors=bcolors)
+			if i == 3:
+				temp, self.aglobalMax, self.data = getCmapByFreqVal(
+					self.data, self.newdata, self.alphaBand, self.aglobalMax)
+				# set colors
+				ccolors = self.pgCM.map(temp)
+				self.alphaGraph.update_nodes(colors=ccolors)
 
-            if i == 3:
-                temp, self.aglobalMax, self.data = getCmapByFreqVal(
-                    self.data, self.newdata, self.alphaBand, self.aglobalMax)
-                # set colors
-                ccolors = self.pgCM.map(temp)
-                self.alphaGraph.update_nodes(colors=ccolors)
+		elapsed = time.time()-starttime
+		self.timer.setInterval(elapsed*100)
+		# set onlclickhover to show power and node label
 
-        elapsed = time.time()-starttime
-        self.timer.setInterval(elapsed*100)
-        # set onlclickhover to show power and node label
-
-    def hideGraph(self, button=None):
+	def hideGraph(self, button=None):
 		fill_1=pg.PlotWidget()
 		fill_1.getPlotItem().hideAxis('bottom')
 		fill_1.getPlotItem().hideAxis('left')
@@ -242,35 +245,31 @@ class EEGmodule_main(QGroupBox):
 		if button.isChecked()==False:
 			if button.text() == "Alpha Band":
 				self.alphaGraph.setParent(None)
-				#self.layout.removeWidget(self.alphaGraph)
-				self.layout.addWidget(fill_1, 0,0,1,1)
+				self.layout.removeWidget(self.alphaGraph)
+				self.layout.addWidget(fill_1, 0,2,1,1)
 				
 			if button.text() == 'Theta Band':
 				self.thetaGraph.setParent(None)
-				#self.layout.removeWidget(self.thetaGraph)
+				self.layout.removeWidget(self.thetaGraph)
 				self.layout.addWidget(fill_2, 0,1,1,1)
 				
-			if button.text() == 'Delta band':
+			if button.text() == "Delta Band":
 				self.deltaGraph.setParent(None)
-				#self.layout.removeWidget(self.deltaGraph)
-				self.layout.addWidget(fill_3, 0,2,1,1)
+				self.layout.removeWidget(self.deltaGraph)
+				self.layout.addWidget(fill_3, 0,0,1,1)
 		else:
 			if button.text() == "Alpha Band" :			
-				self.layout.addWidget(self.alphaGraph,0,0,1,1)
+				self.layout.addWidget(self.alphaGraph,0,2,1,1)
 				fill_1.setParent(None)
-				#self.layout.removeWidget(fill_1)
-				#fill_1.setParent(None)
-				#self.layout.addWidget(self.alphaGraph)
-			if button.text() == 'Delta band' :
-				self.layout.addWidget(self.deltaGraph, 0, 2,1,1)
-				#self.layout.removeWidget(fill_3)
-				fill_3.setParent(None)
-				#self.layout.addWidget(self.deltaGraph)
 			if button.text() == 'Theta Band':
 				self.layout.addWidget(self.thetaGraph, 0,1,1,1)
 				#self.layout.removeWidget(fill_2)
 				fill_2.setParent(None)
 				#self.layout.addWidget(self.thetaGraph)
+			if button.text() == 'Delta Band' :
+				self.layout.addWidget(self.deltaGraph, 0, 0,1,1)
+				#self.layout.removeWidget(fill_3)
+				fill_3.setParent(None)
 
 
 class checkboxes(QGroupBox):
