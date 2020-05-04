19809f16e93a21a2ee4971d08457377116e71d57
William <48535210+WilliamUTD@users.noreply.github.com>
2020-05-04 13:25:49 -0500

    Add files via upload

---
+++ eegModule/EEG_Dashboard_Main.py
@@ -0,0 +1,78 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtMultimedia import *
+from EEGScatterWidget_main import EEGmodule_main
+
+# Subclass QMainWindow to customise your application's main window
+class MainWindow(QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        # set title of application window
+        self.setWindowTitle("Biometric Dashboard")
+		
+        # # resize main window
+        self.resize(1600, 1200)
+
+        # create a window widget for main window
+        widget = QWidget()
+
+        # define a layout for window
+        layout = QGridLayout()
+
+        # add all modules to MainWindow where EEG module takes up 1 row and 2
+        # columns and sits in the top left grid box. The respiration module sits
+        # in the bottom left grid box. The temperature module sits in the bottom
+        # right grid box.
+        layout.addWidget(EEGmodule_main(), 0, 0)
+       
+        #layout.addWidget(DeltaFrequencyPG(), 1, 1)
+
+        # add layout to window Widget
+        widget.setLayout(layout)
+
+        # Set the central widget of the Window. Widget will expand
+        # to take up all the space in the window by default.
+        self.setCentralWidget(widget)
+
+        # change window style to dark mode
+        darkMode()
+
+
+
+	
+# function to change application style to dark mode
+def darkMode():
+    dark_palette = QPalette()
+    dark_palette.setColor(QPalette.Window, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.WindowText, Qt.white)
+    dark_palette.setColor(QPalette.Base, QColor(25, 25, 25))
+    dark_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ToolTipBase, Qt.white)
+    dark_palette.setColor(QPalette.ToolTipText, Qt.black)
+    dark_palette.setColor(QPalette.Text, Qt.white)
+    dark_palette.setColor(QPalette.Button, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ButtonText, Qt.white)
+    dark_palette.setColor(QPalette.BrightText, Qt.red)
+    dark_palette.setColor(QPalette.Link, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.HighlightedText, Qt.black)
+    QApplication.setPalette(dark_palette)
+
+# run application
+if __name__ == '__main__':
+    import sys
+
+    # You need one (and only one) QApplication instance per application.
+    # Pass in sys.argv to allow command line arguments for your app.
+    # If you know you won't use command line arguments QApplication([]) works too.
+    app = QApplication(sys.argv)
+    # create a window from the MainWindow class defined above
+    window = MainWindow()
+    # show the window
+    window.showMaximized()
+    # Start the event loop.
+    sys.exit(app.exec_())
+	
